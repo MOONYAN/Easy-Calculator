00f220197b9f438db101b52be03de1d97514180a
Boruto <t106598010@ntut.org.tw>
2017-10-29 07:57:16 +0800

    add MenuStrip

--- Easy Calculator/MainForm.Designer.cs
+++ Easy Calculator/MainForm.Designer.cs
@@ -36,8 +36,12 @@
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this._monitorLabel = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.calculatorToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.tableLayoutPanel1.SuspendLayout();
             this._commandsTableLayoutPanel.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // tableLayoutPanel1
@@ -45,15 +49,17 @@
             this.tableLayoutPanel1.ColumnCount = 2;
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 75F));
             this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this.tableLayoutPanel1.Controls.Add(this._numbersTableLayoutPanel, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this._commandsTableLayoutPanel, 1, 1);
-            this.tableLayoutPanel1.Controls.Add(this._monitorLabel, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this._numbersTableLayoutPanel, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this._commandsTableLayoutPanel, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this._monitorLabel, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
             this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
             this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 10F));
             this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 80F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 70F));
             this.tableLayoutPanel1.Size = new System.Drawing.Size(718, 553);
             this.tableLayoutPanel1.TabIndex = 0;
             // 
@@ -64,14 +70,14 @@
             this._numbersTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this._numbersTableLayoutPanel.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 33.33333F));
             this._numbersTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._numbersTableLayoutPanel.Location = new System.Drawing.Point(3, 113);
+            this._numbersTableLayoutPanel.Location = new System.Drawing.Point(3, 168);
             this._numbersTableLayoutPanel.Name = "_numbersTableLayoutPanel";
             this._numbersTableLayoutPanel.RowCount = 4;
             this._numbersTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._numbersTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._numbersTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._numbersTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this._numbersTableLayoutPanel.Size = new System.Drawing.Size(532, 437);
+            this._numbersTableLayoutPanel.Size = new System.Drawing.Size(532, 382);
             this._numbersTableLayoutPanel.TabIndex = 0;
             // 
             // _commandsTableLayoutPanel
@@ -84,14 +90,14 @@
             this._commandsTableLayoutPanel.Controls.Add(this.button3, 0, 2);
             this._commandsTableLayoutPanel.Controls.Add(this.button4, 0, 3);
             this._commandsTableLayoutPanel.Dock = System.Windows.Forms.DockStyle.Fill;
-            this._commandsTableLayoutPanel.Location = new System.Drawing.Point(541, 113);
+            this._commandsTableLayoutPanel.Location = new System.Drawing.Point(541, 168);
             this._commandsTableLayoutPanel.Name = "_commandsTableLayoutPanel";
             this._commandsTableLayoutPanel.RowCount = 4;
             this._commandsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._commandsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._commandsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
             this._commandsTableLayoutPanel.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
-            this._commandsTableLayoutPanel.Size = new System.Drawing.Size(174, 437);
+            this._commandsTableLayoutPanel.Size = new System.Drawing.Size(174, 382);
             this._commandsTableLayoutPanel.TabIndex = 1;
             // 
             // button1
@@ -102,7 +108,7 @@
             this.button1.Location = new System.Drawing.Point(20, 20);
             this.button1.Margin = new System.Windows.Forms.Padding(20);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(134, 69);
+            this.button1.Size = new System.Drawing.Size(134, 55);
             this.button1.TabIndex = 0;
             this.button1.Text = "C";
             this.button1.UseVisualStyleBackColor = true;
@@ -113,10 +119,10 @@
             this.button2.AccessibleName = "+";
             this.button2.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.Location = new System.Drawing.Point(20, 129);
+            this.button2.Location = new System.Drawing.Point(20, 115);
             this.button2.Margin = new System.Windows.Forms.Padding(20);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(134, 69);
+            this.button2.Size = new System.Drawing.Size(134, 55);
             this.button2.TabIndex = 1;
             this.button2.Text = "+";
             this.button2.UseVisualStyleBackColor = true;
@@ -127,10 +133,10 @@
             this.button3.AccessibleName = "-";
             this.button3.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.Location = new System.Drawing.Point(20, 238);
+            this.button3.Location = new System.Drawing.Point(20, 210);
             this.button3.Margin = new System.Windows.Forms.Padding(20);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(134, 69);
+            this.button3.Size = new System.Drawing.Size(134, 55);
             this.button3.TabIndex = 2;
             this.button3.Text = "-";
             this.button3.UseVisualStyleBackColor = true;
@@ -141,10 +147,10 @@
             this.button4.AccessibleName = "=";
             this.button4.Dock = System.Windows.Forms.DockStyle.Fill;
             this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button4.Location = new System.Drawing.Point(20, 347);
+            this.button4.Location = new System.Drawing.Point(20, 305);
             this.button4.Margin = new System.Windows.Forms.Padding(20);
             this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(134, 70);
+            this.button4.Size = new System.Drawing.Size(134, 57);
             this.button4.TabIndex = 3;
             this.button4.Text = "=";
             this.button4.UseVisualStyleBackColor = true;
@@ -157,12 +163,38 @@
             this.tableLayoutPanel1.SetColumnSpan(this._monitorLabel, 2);
             this._monitorLabel.Dock = System.Windows.Forms.DockStyle.Fill;
             this._monitorLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this._monitorLabel.Location = new System.Drawing.Point(20, 20);
+            this._monitorLabel.Location = new System.Drawing.Point(20, 75);
             this._monitorLabel.Margin = new System.Windows.Forms.Padding(20);
             this._monitorLabel.Name = "_monitorLabel";
             this._monitorLabel.Size = new System.Drawing.Size(678, 70);
             this._monitorLabel.TabIndex = 2;
             this._monitorLabel.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // menuStrip1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.menuStrip1, 2);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(718, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.calculatorToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // calculatorToolStripMenuItem
+            // 
+            this.calculatorToolStripMenuItem.Name = "calculatorToolStripMenuItem";
+            this.calculatorToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.calculatorToolStripMenuItem.Text = "Calculator";
+            this.calculatorToolStripMenuItem.Click += new System.EventHandler(this.calculatorToolStripMenuItem_Click);
             // 
             // MainForm
             // 
@@ -170,12 +202,15 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(718, 553);
             this.Controls.Add(this.tableLayoutPanel1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "MainForm";
             this.Text = "MainForm";
             this.Load += new System.EventHandler(this.MainForm_Load);
             this.tableLayoutPanel1.ResumeLayout(false);
             this.tableLayoutPanel1.PerformLayout();
             this._commandsTableLayoutPanel.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -190,6 +225,9 @@
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label _monitorLabel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem calculatorToolStripMenuItem;
     }
 }
 
